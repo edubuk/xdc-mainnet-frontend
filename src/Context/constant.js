984aded1ed1a88a554ce8fb3e6fe3eb3a9da269d
ajeetram <86039147+ajeetram@users.noreply.github.com>
2024-10-18 14:24:56 +0530

    edubuk-xdc-ui

--- src/Context/constant.js
+++ src/Context/constant.js
@@ -1,7 +1,7 @@
 import ABI from './edubuksc.json'
 //XDC test network by aproova sir
 //XDC mainnet
-export const EdubukConAdd = "0x384c43441014BC53322566750A890103b212dD9e";
+export const EdubukConAdd = process.env.REACT_APP_Contract;
 //XDC test net
 //export const EdubukConAdd = "0x7c84BB1D231AC10400E58B81C19F875D2E1F8e39";
 export const EdubukConABI = ABI.abi;
